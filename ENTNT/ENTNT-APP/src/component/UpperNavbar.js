9164e04ed6d1d8292beeb029249bf890058190e4
CHAITHANYA SETTY <134689235+SettyChaithanya@users.noreply.github.com>
2024-12-31 18:52:29 +0530

    Update UpperNavbar.js

--- ENTNT/ENTNT-APP/src/component/UpperNavbar.js
+++ ENTNT/ENTNT-APP/src/component/UpperNavbar.js
@@ -6,7 +6,7 @@ const UpperNavbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary uppernavbar">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">ERP Management</Link>
+                    <Link className="navbar-brand" to="/">ENTNT</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
